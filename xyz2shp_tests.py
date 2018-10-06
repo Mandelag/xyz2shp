fb067670d48b4ecc81042fbe55e4a30bd1fe742d
Keenan Gebze <keenan.gebze@gmail.com>
2018-10-06 22:34:46 +0700

    Rename the 'test' method to 'Test' (capital case). Repair some test to use non mutable default min max seed. Repair test data assertion to reflect the changes.

--- xyz2shp_tests.py
+++ xyz2shp_tests.py
@@ -3,23 +3,23 @@ import xyz2shp
 import nose
 
 class Reducer_Test(TestCase):
-    def reducer_test(self):
+    def reducer_Test(self):
         reducer = xyz2shp.Reducer()
-        reducer.register("min_max", xyz2shp.min_max_reducer_default_seed, xyz2shp.min_max_reducer)
+        reducer.register("min_max", xyz2shp.min_max_reducer_default_seed(), xyz2shp.min_max_reducer)
         for i in [1,2,3,4,5,6,7,101,9,10,11]:
             reducer.feed("min_max", i)
         min_max = reducer.get("min_max")
         self.assertEquals(min_max, {"min": 1, "max":101})
 
-    def reducer_register_called_multiple_times_test(self):
+    def reducer_register_called_multiple_times_Test(self):
         reducer = xyz2shp.Reducer()
         for i in [1,2,3,4,5,6,7,101,9,10,11]:
-            reducer.register("min_max", {"min": 20000000, "max": 0}, reducing_logic=xyz2shp.min_max_reducer)
+            reducer.register("min_max",xyz2shp.min_max_reducer_default_seed(), reducing_logic=xyz2shp.min_max_reducer)
             reducer.feed("min_max", i)
         minmax = reducer.get("min_max")
         self.assertEquals(minmax, {"min": 1, "max": 101})
     
-    def simple_count_test(self):
+    def simple_count_Test(self):
         reducer = xyz2shp.Reducer()
         reducer.register("counts", 0, lambda x, y:  x+1)
         test_list = [1,2,3,4,5,6,7,101,9,10,11] 
@@ -33,16 +33,15 @@ class With_Sample_Data_Test(TestCase):
         output_shp = "test_data\\Pitsit_CRST.shp"
         
         feature_statistics = xyz2shp.get_xyz_feature_statistics(input_xyz)
-        print(feature_statistics)
         self.assertEquals(feature_statistics, {
             'vertex_length': {'min': 1, 'max': 3021},
             'counts': 3889,
             'field_set': {'DESCRIPTION', 'DATE', 'ELEVATION', 'LINE', 'LAYER'},
-            'DESCRIPTION_length': {'min': 1, 'max': 3021},
-            'LINE_length': {'min': 1, 'max': 3021},
-            'LAYER_length': {'min': 1, 'max': 3021},
-            'ELEVATION_length': {'min': 1, 'max': 3021},
-            'DATE_length': {'min': 1, 'max': 3021},
+            'DESCRIPTION_length': {'min': 6, 'max': 6},
+            'LINE_length': {'min': 1, 'max': 4},
+            'LAYER_length': {'min': 4, 'max': 4},
+            'ELEVATION_length': {'min': 1, 'max': 4},
+            'DATE_length': {'min': 3, 'max': 14},
             'ring_like': False})
 
     #def get_xyz_features_test(self):
